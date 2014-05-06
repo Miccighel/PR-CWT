37b34d03969c862d14466edbbba398cf9b35f890
Miccighel <miccighel@gmail.com>
2014-05-06 23:45:36 +0200

    eliminazione vecchi file

--- Progetto/html/moduloModificaImmagineIntermedio.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Michael
- * Date: 05/05/14
- * Time: 14.36
- */ 
--- Progetto/script/scriptLogout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../settings/configurazione.inc';
-session_destroy();
-header("location: ../index.php");
-?>
