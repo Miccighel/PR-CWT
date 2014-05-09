cd7727ca51bcf65b581dc159111626ae351f7ecc
Miccighel <miccighel@gmail.com>
2014-05-09 16:24:05 +0200

    Aggiunte eliminazioni, cose belle in js e via dicendo

---
+++ html/moduloEliminazioneConsole.php
@@ -0,0 +1,20 @@
+<?php
+include '../settings/configurazione.inc';
+include HOME_ROOT.'/script/funzioni.php';
+
+if (isset($_SESSION['collegato'])){
+    if ($_SESSION['amministratore'] == true){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            ricercaConsole($_POST['nome'],'../script/scriptEliminazioneConsole.php');
+        } else {
+            include HOME_ROOT . '/html/testa.php';
+            stampaModuloRicerca('moduloEliminazioneConsole.php','console');
+            include HOME_ROOT . '/html/coda.html';
+        }
+    } else {
+        print '<p class="errore">Per poter visualizzare questa pagina devi avere le credenziali da amministratore.</p>';
+    }
+} else {
+    print '<p class="errore">Non sei autorizzato a visualizzare questa pagina, per favore, esegui il login.</p>';
+}
+?>
---
+++ script/scriptEliminazioneConsole.php
@@ -0,0 +1,26 @@
+<?php
+include '../settings/configurazione.inc';
+include HOME_ROOT . '/script/funzioni.php';
+
+if (isset($_SESSION['collegato'])) {
+    if ($_SESSION['amministratore'] == true) {
+        $connessione = creaConnessione(SERVER, UTENTE, PASSWORD, DATABASE);
+
+        $query = sprintf("SELECT COUNT(console) FROM tblprodotticonsole WHERE console='%s'", $_POST['nome']);
+        $dati = eseguiQuery($connessione, $query);
+
+        if(intval($dati[0]['COUNT(console)'] > 0)){
+            print '<p class="informazione">Elimina tutti i prodotti associati a questa console prima di procedere</p>';
+        } else {
+            $query = sprintf("DELETE FROM tblconsole WHERE nome='%s'", $_POST['nome']);
+            $dati = eseguiQuery($connessione, $query);
+            print '<p class="successo">La console e tutti i suoi prodotti sono stati eliminati con successo</p>';
+        }
+        chiudiConnessione($connessione);
+    } else {
+        print '<p class="errore">Non sei autorizzato a visualizzare questa pagina, per favore, esegui il login</p>';
+    }
+} else {
+    print '<p class="errore">Non sei autorizzato a visualizzare questa pagina, per favore, esegui il login</p>';
+}
+?>
