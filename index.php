f2f0664d7ea7100e6f4346c1dd84c20ca4441545
Miccighel <miccighel@gmail.com>
2014-06-02 16:55:17 +0200

    Qualche piccolo aggiustamento

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ include HOME_ROOT.'/html/testa.php';
     <h1>Progetto di "Complementi di Tecnologie Web"</h1>
     <h2>Games Commerce</h2>
     <h3>Soprano Michael - 112151, Perazza Giuliano - 112461</h3>
+    <h4>Tecnologie Web e Multimediali</h4>
+    <h4>Universit&agrave; degli Studi di Udine</h4>
 </div>
 
 <?php
