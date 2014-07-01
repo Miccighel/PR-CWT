3b0763ac05c90d4817ceb89be824f33050f19f07
Michael Soprano <miccighel@gmail.com>
2014-07-01 22:05:44 +0200

    aggiunto session unset allo script logout

--- script/scriptLogout.php
+++ script/scriptLogout.php
@@ -1,5 +1,6 @@
 <?php
 include '../settings/configurazione.inc';
 session_destroy();
+session_unset(); // Elimina le variabili di sessione residenti sul server
 header("location: ../index.php");
 ?>
