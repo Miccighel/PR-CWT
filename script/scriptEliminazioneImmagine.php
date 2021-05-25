ebc1f44824883c5045899fd778674f5819949ccf
Michael Soprano <miccighel@gmail.com>
2021-05-25 14:32:50 +0200

    Update

--- script/scriptEliminazioneImmagine.php
+++ script/scriptEliminazioneImmagine.php
@@ -8,17 +8,17 @@ if($_SERVER['REQUEST_METHOD'] != 'GET'){
     if(count($_POST) > 1){
         foreach ($_POST['immagine'] as $immagine) {
             if (file_exists('../img/thumb/' . $_POST['galleria'] . '/' . $immagine)) {
-                unlink('../img/thumb/' . $_POST['galleria'] . '/' . $immagine);
-            }
+        unlink('../img/thumb/' . $_POST['galleria'] . '/' . $immagine);
+    }
             if (file_exists('../img/' . $_POST['galleria'] . '/' . $immagine)) {
                 unlink('../img/' . $_POST['galleria'] . '/' . $immagine);
             }
             print '<p class="successo">Il file ' . $immagine . ' &egrave; stato eliminato con successo</p>';
         }
-        print '<p class="successo">Hai eliminato con successo ' . count($_POST['immagine']) . ' immagini</p>';
-    } else {
-        print '<p class="informazione">Non &egrave; stata selezionata nessuna immagine da eliminare</p>';
-    }
+print '<p class="successo">Hai eliminato con successo ' . count($_POST['immagine']) . ' immagini</p>';
+} else {
+    print '<p class="informazione">Non &egrave; stata selezionata nessuna immagine da eliminare</p>';
+}
 } else {
     include HOME_ROOT . '/html/testa.php';
     print '<p class="errore">Attenzione, non puoi accedere direttamente a questa pagina</p>';
